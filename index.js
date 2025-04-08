7130c06e8ecb142bc1059f5fbe6697ed6602850e
Emma Acquah <emmaacquah@gmail.com>
2025-04-08 09:33:37 +0100

    Adjust port reference in index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const app = require("./app");
 require('dotenv').config();
 // const port = 3000; // Can remove port because it's included in .env as an environment variable
+const port = process.env.port;
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
