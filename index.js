e413392d90267b1b567e0b4afa602779a394fc49
Emma Acquah <emmaacquah@gmail.com>
2025-04-08 09:22:57 +0100

    Add .env file

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const app = require("./app")
-const port = 3000;
+const app = require("./app");
+require('dotenv').config();
+// const port = 3000; // Can remove port because it's included in .env as an environment variable
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
