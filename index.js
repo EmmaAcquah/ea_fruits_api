487f23cd4689a06bf0b5a611f41286f4a2ee245c
Emma Acquah <emmaacquah@gmail.com>
2025-04-08 09:56:17 +0100

    Correct PORT reference in index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = require("./app");
 require('dotenv').config();
-// const port = 3000; // Can remove port because it's included in .env as an environment variable
-const port = process.env.port;
+
+const port = process.env.PORT;
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
